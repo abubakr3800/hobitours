503560d72bd39dffbb2ac6a0aa21f2527c5d68bd
ahmed abubakr <ahmedabubakr148@gmail.com>
2024-02-23 15:49:05 +0200

    15:49:05.92

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -3,11 +3,8 @@ function checkAuth() {
   if (accessToken) {
   // console.log(`Access token value: ${accessToken}`);
   return true
-  // alert(accessToken)
   } else {
     // console.log('Access token not found.');
-    // alert(accessToken)
-    // location.href = "./sign-in.html"
     return false
   }
 }
@@ -17,27 +14,6 @@ checkAuth() ? "" : location.href="./sign-in.html";
 function logout() {
   localStorage.removeItem("token")
   checkAuth() ? "" : location.href="./sign-in.html";
-
-}
-
-function parseURLParams(url) {
-  var queryStart = url.indexOf("?") + 1,
-      queryEnd   = url.indexOf("#") + 1 || url.length + 1,
-      query = url.slice(queryStart, queryEnd - 1),
-      pairs = query.replace(/\+/g, " ").split("&"),
-      parms = {}, i, n, v, nv;
-
-  if (query === url || query === "") return;
-
-  for (i = 0; i < pairs.length; i++) {
-      nv = pairs[i].split("=", 2);
-      n = decodeURIComponent(nv[0]);
-      v = decodeURIComponent(nv[1]);
-
-      if (!parms.hasOwnProperty(n)) parms[n] = [];
-      parms[n].push(nv.length === 2 ? v : null);
-  }
-  return parms;
 }
 
 // Define a function that returns a promise
@@ -66,27 +42,25 @@ function reqApi(url) {
   });
 }
 
-var languages = ["it" , "en"],
-          allOffers = [];
+var languages = ["it" , "en"], allOffers = [];
 
-var offtab = new DataTable('#offers', {
-    columns: [
-        { title: 'id' },
-        { title: 'offer head' },
-        { title: 'offer' },
-        { title: 'Days' },
-        { title: 'Nights' },
-        { title: 'Language' },
-    ],
-    data: []
-});
-
-
-window.onload = function () {
-  document.getElementById("sidebar").innerHTML = loadPage('sidenav.html');
-
-  // language = (parseURLParams(window.location.href).lan == null || undefined ? "it" : parseURLParams(window.location.href).lan[0] ) ;
-  // getObject("https://hobitours.somee.com/Offer/all/" + language);
+function loadOffers() {
+  document.getElementById("secTitle").innerHTML = 'Watch Offers';
+  document.getElementById('content').innerHTML = `<div class="table-responsive small"><table id="offers" class="display" width="100%"></table></div>`;
+  // Iterate over each language and make an API call for offers in that language
+  if (offtab == null || undefined) {
+    var offtab = new DataTable('#offers', {
+      columns: [
+          { title: 'id' },
+          { title: 'offer head' },
+          { title: 'offer' },
+          { title: 'Days' },
+          { title: 'Nights' },
+          { title: 'Language' },
+      ],
+        data: []
+    });
+  }
   var offersTable ;
   languages.forEach(lan=> {
     offersTable = [] ; 
@@ -112,4 +86,169 @@ window.onload = function () {
   })
   allOffers.push(offersTable)
   // offtab.row.add(allOffers).draw(false);
+}
+
+function manageOffers() {
+  document.getElementById("secTitle").innerHTML = 'Add Offer';
+  var manageForm = document.createElement("form");
+  manageForm.setAttribute('method',"post");
+  manageForm.setAttribute("class" , "row g-3 needs-validation")
+  // manageForm.setAttribute('action','#');
+
+  var enLabel = document.createElement('label');
+  enLabel.setAttribute("class" , "form-label");
+  enLabel.innerText = "offer name in english";
+  var enContainer = document.createElement("div");
+  enContainer.setAttribute("class" , "col-md-6");
+  var enOfferName = document.createElement("input");
+  enOfferName.setAttribute("type" , "text");
+  enOfferName.setAttribute("name" , "enname");
+  enOfferName.setAttribute("class" , "form-control");
+  enContainer.appendChild(enLabel);
+  enContainer.appendChild(enOfferName);
+  manageForm.appendChild(enContainer) ;
+
+  var itLabel = document.createElement('label');
+  itLabel.setAttribute("class" , "form-label");
+  itLabel.innerText = "offer name in italic";
+  var itContainer = document.createElement("div");
+  itContainer.setAttribute("class" , "col-md-6");
+  var itOfferName = document.createElement("input");
+  itOfferName.setAttribute("type" , "text");
+  itOfferName.setAttribute("name" , "itname");
+  itOfferName.setAttribute("class" , "form-control");
+  itContainer.appendChild(itLabel);
+  itContainer.appendChild(itOfferName);
+  manageForm.appendChild(itContainer) ;
+
+  var dLabel = document.createElement('label');
+  dLabel.setAttribute("class" , "form-label");
+  dLabel.innerText = "Days of the offer";
+  var dContainer = document.createElement("div");
+  dContainer.setAttribute("class" , "col-md-6");
+  var days = document.createElement("input");
+  days.setAttribute("type" , "number");
+  days.setAttribute("name" , "days");
+  days.setAttribute("class" , "form-control");
+  dContainer.appendChild(dLabel);
+  dContainer.appendChild(days);
+  manageForm.appendChild(dContainer) ;
+
+  var nLabel = document.createElement('label');
+  nLabel.setAttribute("class" , "form-label");
+  nLabel.innerText = "Nights of the offer";
+  var nContainer = document.createElement("div");
+  nContainer.setAttribute("class" , "col-md-6");
+  var nights = document.createElement("input");
+  nights.setAttribute("type" , "number");
+  nights.setAttribute("name" , "nights");
+  nights.setAttribute("class" , "form-control");
+  nContainer.appendChild(nLabel);
+  nContainer.appendChild(nights);
+  manageForm.appendChild(nContainer) ;
+
+  var endesLabel = document.createElement('label');
+  endesLabel.setAttribute("class" , "form-label");
+  endesLabel.innerText = "Offer Description in English";
+  var endesContainer = document.createElement("div");
+  endesContainer.setAttribute("class" , "col-md-12");
+  var endescrInput = document.createElement("textarea");
+  endescrInput.setAttribute("name" , "endescription");
+  endescrInput.setAttribute("class" , "form-control");
+  endesContainer.appendChild(endesLabel);
+  endesContainer.appendChild(endescrInput);
+  manageForm.appendChild(endesContainer) ;
+
+  var itdesLabel = document.createElement('label');
+  itdesLabel.setAttribute("class" , "form-label");
+  itdesLabel.innerText = "Offer Description in English";
+  var itdesContainer = document.createElement("div");
+  itdesContainer.setAttribute("class" , "col-md-12");
+  var itdescrInput = document.createElement("textarea");
+  itdescrInput.setAttribute("name" , "itdescription");
+  itdescrInput.setAttribute("class" , "form-control");
+  itdesContainer.appendChild(itdesLabel);
+  itdesContainer.appendChild(itdescrInput);
+  manageForm.appendChild(itdesContainer) 
+
+  var btnSubmit = document.createElement('button');
+  btnSubmit.setAttribute('type' , 'submit');
+  btnSubmit.setAttribute('class' , 'btn btn-primary col-12');
+  btnSubmit.innerHTML = "Add Offer";
+
+  btnSubmit.addEventListener('click' , function (event) {
+    event.preventDefault();
+    addNewOffer(manageForm);
+  } )
+
+  manageForm.appendChild(btnSubmit);
+
+  document.getElementById('content').innerHTML = ``;
+  document.getElementById('content').appendChild(manageForm) ;
+
+
+}
+
+class Offer {
+  name;
+  day_night;
+  description;
+  image;
+  languageCode;
+}
+
+function addNewOffer(off) {
+  // console.log(ee.enname.value);
+  var enOffer = new Offer(),
+      itOffer = new Offer();
+  
+      enOffer.name= off.enname.value;
+      enOffer.day_night = Number(off.days.value) + "," + Number(off.nights.value);
+      enOffer.description = off.endescription.value;
+      enOffer.image = "";
+      enOffer.languageCode = "en";
+
+      itOffer.name= off.itname.value;
+      itOffer.day_night = Number(off.days.value) + "," + Number(off.nights.value);
+      itOffer.description = off.itdescription.value;
+      itOffer.image = "";
+      itOffer.languageCode = "it";
+
+      var newOffer = [enOffer,itOffer];
+      newOffer = JSON.stringify(newOffer);
+
+  postApi( 'https://hobitours.somee.com/Offer/add' , newOffer)
+  console.log(newOffer);
+  loadOffers()
+}
+
+window.onload = function () {
+  document.getElementById("sidebar").innerHTML = loadPage('sidenav.html');
+
+  // language = (parseURLParams(window.location.href).lan == null || undefined ? "it" : parseURLParams(window.location.href).lan[0] ) ;
+  // getObject("https://hobitours.somee.com/Offer/all/" + language);
+  
+}
+
+// Define a function that returns a promise
+function postApi(url,str_json) {
+    
+  // Send the JSON data to your PHP script
+  // var url = 'capilot.php';
+  fetch(url, {
+    method: 'POST',
+    body: str_json,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(
+      (response) => {
+          response.text()
+          return response
+      })
+  .then(txt => {
+    // Handle the response from the server
+    console.log(txt);
+  });
 }
--- dashboard/js/sec.js
+++ dashboard/js/sec.js
@@ -10,8 +10,66 @@ class User {
 }
 
 var logUser = new User() ;
+
 document.getElementById("logForm").onsubmit = function (e) {
     e.preventDefault();
-    localStorage.setItem("token" , "112233")
-    location.href = "./index.html"
+    // localStorage.setItem("token" , "112233")
+    // location.href = "./index.html"
+
+    // console.log(this.email.value);
+    if (this.email.value == "" || this.password.value == "") {
+        alert("please fill boxes with right info");
+    }else {
+        var userData ={
+            email : this.email.value,
+            password: this.password.value
+        };
+        // Convert the object to a JSON string
+        var str_json = JSON.stringify(userData);
+        // console.log(str_json);
+        var signUser = signApi("https://hobitours.somee.com/user/login/" , str_json);
+        console.log(signUser);
+        signUser.then((result)=>{
+            console.log(result);
+            if (result.status === 400) {
+                alert('Wrong Email or Password');
+            } else if (result.status === 500) {
+                alert('Server Error')
+            } else {
+                logUser.token = result.token;
+                logUser.name = result.data.username;
+                logUser.id= result.data._id;
+                logUser.exDate = new Date().getTime() + parseInt(result.data.expiresIn);
+                setCookie("token", logUser.token , logUser.exDate );
+                setCookie("userId", logUser.id , logUser.exDate );
+                window.location.replace("./index.html");
+            }
+        })
+         .catch((err)=>console.log(err));
+    }
+
 }
+
+
+// Define a function that returns a promise
+function signApi(url,str_json) {
+    
+    // Send the JSON data to your PHP script
+    // var url = 'capilot.php';
+    fetch(url, {
+      method: 'POST',
+      body: str_json,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(
+        (response) => {
+            response.text()
+            return response
+        })
+    .then(txt => {
+      // Handle the response from the server
+      console.log(txt);
+    });
+  }
